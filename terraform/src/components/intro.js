2989e2538c472cb34abcd2972b59514cba27b24a
Christina Harris <christinathedesigner@outlook.com>
2020-06-26 09:20:51 -0700

    fixed intro

--- terraform/src/components/intro.js
+++ terraform/src/components/intro.js
@@ -26,27 +26,30 @@ const Intro = () => {
         a grid of squares called cells, each cell is black (alive) or white
         (dead). When people are playing the game, they start by making some dead
         cells alive. Then the alive cells can make other cells alive or dead
-        because of the rules of the game. The rules are:[1] Any alive cell that
-        is touching less than two alive neighbors dies. Any alive cell touching
-        four or more alive neighbors dies. Any alive cell touching two or three
-        alive neighbors does nothing. Any dead cell touching exactly three alive
-        neighbors becomes alive. The way a player sets up the first alive cells
-        starts the game. The first level (generation) is made by using the 4
-        rules. After the player is done changing the cells, they do the same
-        thing all over again to make the next level. The game is over when all
-        the cells are dead or all the cells are alive or the game is doing the
-        same thing over and over.{" "}
+        because of the rules of the game. The rules are:
       </Para>
-
+      <br />
       <Para>
-        {" "}
+        &bull; Any alive cell that is touching less than two alive neighbors
+        dies.
+        <br />
+        &bull; Any alive cell touching four or more alive neighbors dies.
+        <br />
+        &bull;Any alive cell touching two or three alive neighbors does nothing.
+        <br />
+        &bull;Any dead cell touching exactly three alive neighbors becomes
+        alive.{" "}
+      </Para>
+      <Para>
+        The way a player sets up the first alive cells starts the game. The
+        first level (generation) is made by using the 4 rules. After the player
+        is done changing the cells, they do the same thing all over again to
+        make the next level. The game is over when all the cells are dead or all
+        the cells are alive or the game is doing the same thing over and over.
         Terraform brings a new twist to the Game of Life. Your task is to
         terraform a planet with nanobots. You will be in charge of bot
-        placement. But that's not all. The four basic elements that all life
-        needs is Carbon, Oxygen, Hydrogen, Nitrogen. Your task is to terraform a
-        planet by placing these elements onto a planet with nanobots to be
-        terraformed. All planets have different conditions that will effect your
-        nanobots differently. etc.etc{" "}
+        placement. But that's not all. Your task is to terraform a planet by
+        placing nanobots to transform carbons to water.{" "}
       </Para>
       <Container>
         <AwesomeButton onClick={routeChange}>Proceed</AwesomeButton>
